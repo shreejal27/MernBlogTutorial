06a7491b686aedc226c9506aef49205fb74abd94
Shreejal Maharjan <shreejal27@gmail.com>
2023-12-02 13:07:43 +0545

    made a home component

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+        <h1>Home</h1>
+        <p>Lorem ipsul dolor sit amt consectetur adipisicing elit. Rem esse, ste consectetur totam sit et 
+        voluptatum, quibusdam, quidem, quas voluptates quos. Quisquam, quod. Quisquam, quod. Quisquam, quod.
+        </p>
+        <p>Lorem ipsul dolor sit amt consectetur adipisicing elit. Rem esse, ste consectetur totam sit et 
+        voluptatum, quibusdam, quidem, quas voluptates quos. Quisquam, quod. Quisquam, quod. Quisquam, quod.
+        sdf jioj foiw ei o woijefi ow oiwj f jieiieie ojovsmmsdf owjefoilsj dfo oij efoijwef
+        </p>
+        <p>Lorem ipsul dolor sit amt consectetur adipisicing elit. Rem esse, ste consectetur totam sit et 
+        voluptatum, quibusdam, quidem, quas voluptates quos. Quisquam, quod. Quisquam, quod. Quisquam, quod.
+        </p>
+        </div>
+    );
+}
+
+export default Home
