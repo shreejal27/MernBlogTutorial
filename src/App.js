5acf180dc688de042fb31fcbc61dd3c95fb4e25d
Shreejal Maharjan <shreejal27@gmail.com>
2023-12-02 13:12:55 +0545

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
