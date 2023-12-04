ae39daa06ecad9d18c9f5940e2e0ba21ea547857
Shreejal Maharjan <shreejal27@gmail.com>
2023-12-04 19:06:39 +0545

    linking the articles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/articleslist">Article List</Link></li>
-                <li><Link to="/article">Article</Link></li>
+                {/* <li><Link to="/article">Article</Link></li> */}
             </ul>
         </nav>
     )
--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,28 +1,33 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 import articleContent from "./article-content";
 
 const ArticlesList = () => {
     return (
         <div>
-        <h1>Articles List</h1>
-        <p>Lorem ipsul dolor sit amt consectetur adipisicing elit. Rem esse, ste consectetur totam sit et 
-        voluptatum, quibusdam, quidem, quas voluptates quos. Quisquam, quod. Quisquam, quod. Quisquam, quod.
-        </p>
-        <div>
+            <h1>Articles List</h1>
+            <p>Lorem ipsul dolor sit amt consectetur adipisicing elit. Rem esse, ste consectetur totam sit et
+                voluptatum, quibusdam, quidem, quas voluptates quos. Quisquam, quod. Quisquam, quod. Quisquam, quod.
+            </p>
             <div>
-                {articleContent.map((article,index)=>(
-                    <div key={index}>
-                        <div>
-                        <Link to={`/article/${article.name}`}>
-                        <img className="thumbnail" src={article.thumbnail} alt="thumbnail"/>
-                        </Link>
+                <div>
+                    {articleContent.map((article, index) => (
+                        <div key={index}>
+                            <div>
+                                <Link to={`/article/${article.name}`}>
+                                    <img className="thumbnail" src={article.thumbnail} alt="thumbnail" />
+                                </Link>
+                                <div>
+                                    <h3>{article.title}</h3>
+                                    <p>{article.content[0].substring(0, 150)}...</p>
+                                    <Link to={`/article/${article.name}`}>Read more</Link>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
         </div>
     );
 }
