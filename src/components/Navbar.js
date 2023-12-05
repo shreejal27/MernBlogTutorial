4361ee354602e232938f1200da19915767407e1b
Shreejal Maharjan <shreejal27@gmail.com>
2023-12-05 21:38:08 +0545

    added css to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import './Navbar.css'
+
 const Navbar = () => {
     return (
         <nav>
+            <p id='title'>MyBlogs</p>
             <ul>
                 {/* <li><a href="/">Home</a></li> */}
                 <li><Link to="/">Home</Link></li>
